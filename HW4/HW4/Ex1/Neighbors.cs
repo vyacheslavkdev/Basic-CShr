9b82b7c6a522b513f05c30c940aa80ec1c9d6f1b
VyacheslavK <vyacheslavkdev@gmail.com>
2020-10-04 18:50:44 +0300

    HW4

---
+++ HW4/HW4/Ex1/Neighbors.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Вячеслав Индорил
+ * Задача 1
+    Дан  целочисленный  массив  из 20 элементов.  Элементы  массива  могут принимать  целые  
+    значения  от –10 000 до 10 000 включительно. Заполнить случайными числами.  Написать программу, 
+    позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3. 
+    В данной задаче под парой подразумевается два подряд идущих элемента массива. Например, 
+    для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
+ */
+
+using System;
+
+namespace Ex1
+{
+    class Neighbors
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            InitArray(array);
+            Calculate(array);
+        }
+
+        private static void Calculate(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var a = array[i];
+                var b = array[i + 1];
+                if ((a % 3 == 0 && b % 3 != 0)
+                    || (a % 3 != 0 && b % 3 == 0))
+                {
+                    Console.Write($"[{a} {b}] ");
+                    count++;
+                }
+
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine($"\nКоличество соседних элементов, в котором только один делится на 3 = {count}");
+        }
+
+        private static void InitArray(int[] array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10000, 10000);
+
+                Console.Write("{0, 7}", array[i]);
+
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HW4/HW4/Ex5/MyMatrixWork.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Вячеслав Индорил
+ * Задача 5
+    *а) Реализовать библиотеку с классом для работы с двумерным массивом.
+        Реализовать конструктор, заполняющий массив случайными числами. Создать методы, 
+        которые возвращают сумму всех элементов массива, сумму всех элементов массива больше заданного, 
+        свойство, возвращающее минимальный элемент массива, свойство, возвращающее максимальный элемент массива, 
+        метод, возвращающий номер максимального элемента массива (через параметры, используя модификатор ref или out).
+    **б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+    **в) Обработать возможные исключительные ситуации при работе с файлами.
+ */
+
+using System;
+using MyMatrixNamespace;
+
+namespace Ex5
+{
+    class MyMatrixWork
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int rows = rnd.Next(3, 6);
+            int cols = rnd.Next(5, 11);
+            int initVal = rnd.Next(-10, 11);
+            int valStep = rnd.Next(1, 11);
+            Console.WriteLine($"Инициализация MyMatrix. " +
+                              $"Количество строк = {rows}, Количество столбцов = {cols}, " +
+                              $"Начальное значение = {initVal}, Шаг до +- {valStep}");
+            
+            MyMatrix myMatrix = new MyMatrix(rows, cols, initVal, valStep);
+            myMatrix.Show();
+            
+            Console.WriteLine($"Сумма всех элементов массива = {myMatrix.ElementSum()}");
+            Console.WriteLine($"Сумма всех элементов массива больше {valStep / 2} = {myMatrix.ElementSum(valStep / 2)}");
+            Console.WriteLine($"Минимальный элемент массива = {myMatrix.MinElement}, " +
+                              $"Максимальный элемент = {myMatrix.MaxElement}");
+            int maxRowId = 0;
+            int maxColId = 0;
+            myMatrix.SetMaxElementId(ref maxRowId, ref maxColId);
+            Console.WriteLine($"Порядковый номер максимального элемента массива = [{maxRowId}][{maxColId}]");
+
+            Console.WriteLine("\nИнициализация MyMatrix из файла.");
+            
+            string fileName = "../../matrix.txt";
+            MyMatrix myFileMatrix = new MyMatrix(fileName);
+            myFileMatrix.Show();
+        }
+    }
+}
